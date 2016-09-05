128fcada8e21058011c0dfae92c64064bb5d3d38
DoItMorty <sebastian@ssmithcis.net>
2016-09-05 02:03:00 -0700

    100% more python

--- ingen.py
+++ ingen.py
@@ -1,7 +1,9 @@
 import argparse
 import time
+from pylatex import Document, Package, Command
+from pylatex.utils import NoEscape
 
-#Configurating argparse
+#Command line arguments
 parser = argparse.ArgumentParser(description='SeaSquids Invoice Generator',
     usage='Havent quite figured it out yet')
 
@@ -15,69 +17,43 @@ parser.add_argument('--myemail',help='Your Email')
 parser.add_argument('--clientname',help='Recipent Name')
 parser.add_argument('--clientaddress',help='street;zip')
 parser.add_argument('--notes',help='Additional Notes')
-
-
 args = parser.parse_args()
+
 
 date = time.strftime("%m/%d/%y")
 
-myaddress = args.myaddress
-myaddrsplit = myaddress.split(';')
-mystreet = myaddrsplit[0]
-myzip = myaddrsplit[1]
+myaddress = args.myaddress.split(';')
+clientaddress = args.clientaddress.split(';')
+
+invoiceNum = str(9999)
 
 
-clientaddress = args.clientaddress
-clientaddrsplit = clientaddress.split(';')
-clientstreet = clientaddrsplit[0]
-clientzip = clientaddrsplit[1]
+doc = Document(documentclass='letter')
+doc.packages.append(Package('invoice'))
+
+#This needs work to fix backticks 
+doc.preamble.append(Command('address', args.myname  + myaddress[0] \
+	+ myaddress[1] + args.myemail))
+doc.preamble.append(Command('date', date))
+
+doc.append(Command(command='begin', arguments='letter', \
+	extra_arguments=args.clientname + clientaddress[0] + \
+	clientaddress[1]))
 
 
-template = """
-\documentclass{letter}
-\usepackage{invoice} 
-\\address{%myname \\\\ %mystreet \\\\ %myzip \\\\ %myemail} 
-\date{%date}
+doc.append(Command(command='opening', arguments='Invoice ' + invoiceNum))
+doc.append(Command(command='begin', arguments='invoice', extra_arguments=['USD','0']))
+doc.append(Command(command='ProjectTitle', arguments=args.title))
 
-\\begin{document}
-\\begin{letter}{%clientname \\\\ %clientstreet \\\\ %clientzip}
-\opening{invoice %invoicenumber}
-\\begin{invoice}{USD}{0}
-\ProjectTitle{%title}
-
-\\fee{%description} {%unitprice} {%quantity}
-
-\end{invoice}
-\closing{%notes}
-\end{letter}
-\end{document}
-
-"""
-
-for line in template:
-    l1 = template.replace('%date',date)
-    l2 = l1.replace('%title',args.title)
-    l3 = l2.replace('%myname',args.myname)
-    l4 = l3.replace('%mystreet',mystreet)
-    l5 = l4.replace('%myzip',myzip)
-    l6 = l5.replace('%myemail',args.myemail)
-    l7 = l6.replace('%clientname',args.clientname)
-    l8 = l7.replace('%clientstreet',clientstreet)
-    l9 = l8.replace('%clientzip',clientzip)
-    l10 = l9.replace('%title',args.title)
-    l11 = l10.replace('%notes',args.notes)
+#Fee
 
 
 
+doc.append(Command(command='end', arguments='invoice'))
+doc.append(Command(command='closing', arguments=args.notes))
+doc.append(Command(command='end', arguments='letter'))
+doc.generate_tex()
 
 
-for i in range(len(args.line)):
-    split1 = args.line[i]
-    newsplit = split1.split(';')
-    
-
-    print newsplit[0] + newsplit[1] + newsplit[2]
-    
-    
 
 
